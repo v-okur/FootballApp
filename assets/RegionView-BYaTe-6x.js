d1623866a448e16b7cecfdbaee6841b8827e178c
v-okur <vahdettinokur@gmail.com>
2024-07-23 16:55:47 +0300

    Updates

--- assets/RegionView-BYaTe-6x.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-Zi1uKcqB.js";const o={};function a(r,t){return n(),c("div",null,"xxxxaaa")}const _=e(o,[["render",a]]);export{_ as default};
--- assets/TeamView-cQglJ6Js.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as s,o}from"./index-Zi1uKcqB.js";const t={};function a(c,n,r,f,_,d){return o(),s("div",null,"sdfasfdsfdasf")}const i=e(t,[["render",a]]);export{i as default};
