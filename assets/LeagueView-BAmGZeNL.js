01fccbacc06bdcfe2d2c18c9e4b36f677c7eac22
v-okur <vahdettinokur@gmail.com>
2024-07-23 16:29:50 +0300

    Updates

--- assets/LeagueView-BAmGZeNL.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-Dql4ZsZz.js";const o={};function r(t,a){return n(),c("div",null,"xxxx")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/RegionView-D_U0HVl3.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index-CPoU0Ekz.js";const o={};function a(r,t){return n(),c("div",null,"xxxxaaa")}const _=e(o,[["render",a]]);export{_ as default};
