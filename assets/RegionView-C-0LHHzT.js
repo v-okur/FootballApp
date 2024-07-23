dc523243c0cdd20e273dcf29a4d7672474f978ad
v-okur <vahdettinokur@gmail.com>
2024-07-23 17:00:46 +0300

    Updates

---
+++ assets/RegionView-C-0LHHzT.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index-B-opE4Hz.js";const o={};function a(r,t){return n(),c("div",null,"xxxxaaa")}const _=e(o,[["render",a]]);export{_ as default};
---
+++ assets/TeamView-BCaw5vnZ.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o}from"./index-B-opE4Hz.js";const t={};function a(c,n,r,f,_,d){return o(),s("div",null,"sdfasfdsfdasf")}const i=e(t,[["render",a]]);export{i as default};
